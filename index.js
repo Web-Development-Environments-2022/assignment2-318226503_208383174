5b04ffdc2c2469c609799ab78946d640825e065d
ShahafErez <Shahaf30@gmail.com>
2022-04-29 20:29:18 +0300

    layout

---
+++ index.js
@@ -0,0 +1,41 @@
+class User {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
+}
+let users = [];
+
+$(document).ready(function () {
+  users.push(new User("k", "k"));
+});
+
+function goToLogin() {
+  const loginTab = $(".loginTab");
+  switchToTab(loginTab);
+}
+
+function goToRegister() {
+  const registerTab = $(".registerTab");
+  switchToTab(registerTab);
+}
+function goToWelcome() {
+  const registerTab = $(".welcomeTab");
+  switchToTab(registerTab);
+}
+
+function goToGame() {
+  const registerTab = $(".gameTab");
+  switchToTab(registerTab);
+  StartGame();
+}
+
+function goToAbout() {
+  const registerTab = $(".aboutTab");
+  switchToTab(registerTab);
+}
+
+function switchToTab(newTab) {
+  $(".tabContainer").removeClass("activeTab");
+  $(newTab).addClass("activeTab");
+}
