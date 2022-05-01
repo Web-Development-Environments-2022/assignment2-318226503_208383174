17ee5946114bb2dd24184952908ed24241c59208
ShahafErez <Shahaf30@gmail.com>
2022-05-01 20:34:26 +0300

    fixed some bugs- need to fix restart after fail

--- game.js
+++ game.js
@@ -2,12 +2,15 @@ var context;
 var shape = new Object();
 var board;
 var score;
+var lives;
 var pac_color;
 var start_time;
 var time_elapsed;
 var interval;
 var monsters_locations;
 var monsters_remain;
+const monster = 5;
+const wasFoodNowMonster = 6;
 
 // $(document).ready(function() {
 // 	context = canvas.getContext("2d");
@@ -22,6 +25,7 @@ function StartGame() {
 function Start() {
   board = new Array();
   score = 0;
+  lives = 5;
   pac_color = "yellow";
   var cnt = 100;
   var food_remain = 50;
@@ -45,7 +49,7 @@ function Start() {
     var location = monsters_start_locations[i];
     var x = location[0];
     var y = location[1];
-    board[x][y] = 5;
+    board[x][y] = monster;
     monsters_locations[i] = [x, y];
   }
 
@@ -101,7 +105,8 @@ function Start() {
     },
     false
   );
-  interval = setInterval(UpdatePosition, 500); // changed from 250
+  interval = setInterval(UpdatePosition, 90); // changed from 250
+  interval = setInterval(updateEnemyPosition, 500); // changed from 250
 }
 
 function findRandomEmptyCell(board) {
@@ -132,6 +137,7 @@ function GetKeyPressed() {
 function Draw() {
   canvas.width = canvas.width; //clean board
   lblScore.value = score;
+  lblLives.value = lives;
   lblTime.value = time_elapsed;
   for (var i = 0; i < 10; i++) {
     for (var j = 0; j < 10; j++) {
@@ -167,7 +173,7 @@ function Draw() {
         context.fill();
       }
       // - monster -
-      else if (board[i][j] == 5) {
+      else if (board[i][j] == monster || board[i][j] == wasFoodNowMonster) {
         context.beginPath();
         context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
         context.lineTo(center.x, center.y);
@@ -183,7 +189,7 @@ function Draw() {
 }
 
 function UpdatePosition() {
-  updateEnemyPosition();
+  // updateEnemyPosition();
   board[shape.i][shape.j] = 0;
   var x = GetKeyPressed();
   if (x == 1) {
@@ -221,43 +227,93 @@ function UpdatePosition() {
   } else {
     Draw();
   }
+}
 
-  function updateEnemyPosition() {
-    for (var i = 0; i < monsters_remain; i++) {
-      var location = monsters_locations[i];
-      var x = location[0];
-      var y = location[1];
+function updateEnemyPosition() {
+  // debugger;
+  for (var i = 0; i < monsters_remain; i++) {
+    var location = monsters_locations[i];
+    var x = location[0];
+    var y = location[1];
 
-      // checking if the monster should go vertically or horizontally
-      x_dist = Math.abs(shape.i - x);
-      y_dist = Math.abs(shape.j - y);
+    // checking if the monster should go vertically or horizontally
+    x_dist = Math.abs(shape.i - x);
+    y_dist = Math.abs(shape.j - y);
 
-      // should move on the x axis
-      if (x_dist > y_dist) {
-        // move right
-        if (x < shape.i) {
-          monsters_locations[i][0] = monsters_locations[i][0] + 1;
-          board[x + 1][y] = 5;
-        }
-        // move left
-        else {
-          monsters_locations[i][0] = monsters_locations[i][0] - 1;
-          board[x - 1][y] = 5;
-        }
-      } else {
-        // move down
-        if (y < shape.j) {
-          monsters_locations[i][1] = monsters_locations[i][1] + 1;
-          board[x][y + 1] = 5;
-        }
-        // move up
-        else {
-          monsters_locations[i][1] = monsters_locations[i][1] - 1;
-          board[x][y - 1] = 5;
-        }
+    // trying to get the best move without hitting a wall
+    var move_result;
+    if (x_dist > y_dist) {
+      // should move horizontaly
+      move_result = move_horizontaly_monster(x, y, i);
+      if (move_result == false) {
+        move_diagonally_monster(x, y, i);
       }
+    } else {
+      move_result = move_diagonally_monster(x, y, i);
+      if (move_result == false) {
+        move_horizontaly_monster(x, y, i);
+      }
+    }
+    if (board[x][y] == wasFoodNowMonster) {
+      board[x][y] = 1;
+    } else {
       board[x][y] = 0;
     }
-    // Draw();
+  }
+  // Draw();
+}
+
+function move_horizontaly_monster(x, y, i) {
+  var new_location;
+  // move right
+  if (x < shape.i) {
+    new_location = [x + 1, y];
+  }
+  // move left
+  else {
+    new_location = [x - 1, y];
+  }
+  return move_monster(new_location, i);
+}
+
+function move_diagonally_monster(x, y, i) {
+  // move down
+  if (y < shape.j) {
+    new_location = [x, y + 1];
+  }
+  // move up
+  else {
+    new_location = [x, y - 1];
+  }
+  return move_monster(new_location, i);
+}
+
+function move_monster(new_location, i) {
+  debugger;
+  var new_x = new_location[0];
+  var new_y = new_location[1];
+  if (board[new_x][new_y] == 4) {
+    return false;
+  }
+  // food
+  if (board[new_x][new_y] == 1) {
+    board[new_x][new_y] = wasFoodNowMonster;
+  } else if (board[new_x][new_y] == 2) {
+    // packman
+    eatenByMonster();
+  } else {
+    board[new_x][new_y] = monster;
+  }
+  monsters_locations[i] = new_location;
+  return true;
+}
+
+function eatenByMonster() {
+  if (lives > 0) {
+    score -= 10;
+    lives -= 1;
+    // TODO- restart game
+  } else {
+    alert("game over"); //TODO
   }
 }
